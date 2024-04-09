1b4c7dd35d61bddfd40eb88380cbc1e83e01a045
Samuel Wu <cernunnos1710@gmail.com>
2024-04-09 01:09:42 +0100

    changed Tuple to List for mutability. added more methods to tictactoeboard

--- engine/board.py
+++ engine/board.py
@@ -1,9 +1,9 @@
 from node import Node
-from typing import Tuple
+from typing import List
 
 class Board(Node):
     def __init__(self):
         self.terminal = False
         self.winner = None
         self.turn = True
-        self.space = Tuple()
+        self.space = List()
--- games/tictactoe.py
+++ games/tictactoe.py
@@ -1,8 +1,39 @@
-from typing import Tuple
+from board import Board
+from typing import List, Optional, Set
 
 class TicTacToeBoard(Board):
     def __init__(self):
         super().__init__()
-        self.space = Tuple((None, None, None),
-                      (None, None, None),
-                      (None, None, None))
+        self.space: List[List[Optional[bool]]] = [[None, None, None]
+                                                  for _ in range(3)]
+
+    def deep_copy(self) -> 'TicTacToeBoard':
+        copy = TicTacToeBoard()
+        copy.space = [row[:] for row in self.space]
+        copy.terminal = self.terminal
+        copy.winner = self.winner
+        copy.turn = self.turn
+        return copy
+
+    def find_successors(self) -> Set['TicTacToeBoard']:
+        successors = set()
+        if self.terminal:
+            return successors
+        # TODO: add this lol
+        return successors
+
+    def find_rand_successor(self) -> Optional['TicTacToeBoard']:
+        if self.terminal:
+            return None
+        # TODO: complete this lol
+        return None
+
+    def is_terminal(self) -> bool:
+        return self.terminal
+
+    def get_reward(self) -> float:
+        if not self.terminal:
+            return 0.0  # Game has not finished
+        if self.winner is None:
+            return 0.5  # Draw
+        return 1.0 if (self.winner == self.turn) else 0.0
