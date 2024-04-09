61a5a0fa48a63662df6890839913a3c24e799b53
Samuel Wu <cernunnos1710@gmail.com>
2024-04-09 17:16:46 +0100

    complete find_successors and find_rand_successor methods in tictactoe board

--- games/tictactoe.py
+++ games/tictactoe.py
@@ -1,5 +1,6 @@
 from board import Board
-from typing import List, Optional, Set
+from typing import List, Optional, Set, Tuple
+import random
 
 class TicTacToeBoard(Board):
     def __init__(self):
@@ -7,26 +8,44 @@ class TicTacToeBoard(Board):
         self.space: List[List[Optional[bool]]] = [[None, None, None]
                                                   for _ in range(3)]
 
-    def deep_copy(self) -> 'TicTacToeBoard':
+    def _deep_copy(self) -> 'TicTacToeBoard':
         copy = TicTacToeBoard()
-        copy.space = [row[:] for row in self.space]
         copy.terminal = self.terminal
         copy.winner = self.winner
         copy.turn = self.turn
+        copy.space = [row[:] for row in self.space]
         return copy
 
+    def _update_state(self):
+        # TODO: check if win or not
+        self.turn = not self.turn
+
     def find_successors(self) -> Set['TicTacToeBoard']:
-        successors = set()
+        successors: Set['TicTacToeBoard'] = set()
         if self.terminal:
             return successors
-        # TODO: add this lol
+        for i in range(3):
+            for j in range(3):
+                if self.space[i][j] is not None:
+                    continue
+                succ = self._deep_copy()
+                succ.space[i][j] = self.turn
+                succ._update_state()
+                successors.add(succ)
         return successors
 
     def find_rand_successor(self) -> Optional['TicTacToeBoard']:
         if self.terminal:
             return None
-        # TODO: complete this lol
-        return None
+        empty_cells: List[Tuple[int, int]] = [(i, j) for j in range(3)
+                       for i in range(3) if self.space[i][j]]
+        if not empty_cells:
+            return None
+        move: Tuple[int, int] = random.choice(empty_cells)
+        succ = self._deep_copy()
+        succ.space[move[0]][move[1]] = self.turn
+        succ._update_state()
+        return succ
 
     def is_terminal(self) -> bool:
         return self.terminal
